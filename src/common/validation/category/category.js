2853155bc9cb0d27e3d0530354a3959218d6c317
RafiAkem <yc66zio@gmail.com>
2025-06-06 15:08:14 +0700

    Fixed: Category

--- src/common/validation/category/category.js
+++ src/common/validation/category/category.js
@@ -1,10 +1,12 @@
 const Joi = require("joi");
 
 const createCategorySchema = Joi.object({
+  userId: Joi.string().uuid().required(),
   name: Joi.string().required().min(1).max(100),
 });
 
 const updateCategorySchema = Joi.object({
+  userId: Joi.string().uuid().required(),
   name: Joi.string().min(1).max(100),
 });
 
